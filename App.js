e1cf0251649c08bc73029dd5b92d8e9a58cd416a
fredcoutinho52 <42944398+fredcoutinho52@users.noreply.github.com>
2021-07-12 18:05:52 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import api from "./services/api";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [apiData, setApiData] = useState([]);
+
+    async function getData() {
+        const response = await api.get("facts");
+        const el = response.data.filter(fact => fact._id === "5887e1d85c873e0011036889");
+        console.log(el);
+        setApiData(response.data);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {apiData.map(fact => (
+                <Text key={fact._id}>{fact.text}</Text>
+            ))}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
